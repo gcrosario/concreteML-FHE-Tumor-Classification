409bc52515d5eb8ebc221a58ab97d97a07e727a7
gcrosario <gwynethrosario@gmail.com>
2023-06-03 18:55:19 +0800

    fixed frontend, organized files

--- web_project/urls.py
+++ web_project/urls.py
@@ -21,7 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tumorClassifier.urls', 
-                    #  namespace='classifier'
-                     )),
+    path('', include('tumorClassifier.urls',)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
